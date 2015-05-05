5e156b5cd26bf4d851e29a150fb8166a2158c8b7
Džemal Čengić <dcengic2@etf.unsa.ba>
2015-05-05 20:33:32 +0200

    Projekat je kreiran

---
+++ Kino/PočetnaLoginForma/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BlagajnikForme;
+using FinansijskiMenadžerForme;
+using AdministratorForme;
+
+namespace PočetnaLoginForma
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(username.Text == "blagajnik")
+            {
+                var prozor = new BlagajnikPočetna();
+                this.Close();
+                prozor.ShowDialog();
+                //exit();
+            }
+            else if (username.Text == "finansije")
+            {
+                var prozor = new FinansijskiMenadžerPočetna(); //Proslijedit logovanog korisnika
+                this.Close();
+                prozor.ShowDialog();
+                //exit(); 
+            }
+            else
+            {
+                var prozor = new AdministratorPočetna();
+                this.Close();
+                prozor.ShowDialog();
+                //exit();
+            }
+           
+        }
+    }
+}
