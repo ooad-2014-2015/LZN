393b44d8d3c5257faacbb7652d12eba2de6e8683
adnatahic <atahic1@etf.unsa.ba>
2015-05-15 11:50:38 +0200

    Slike ubacene

---
+++ Igrica_OOAD/Assets/Skripte/Kokica.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kokica : MonoBehaviour {
+
+	public int speed= 6;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
