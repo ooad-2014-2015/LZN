850b6138e9f598a0115368f67cc72a51596cee91
Džemal Čengić <dcengic2@etf.unsa.ba>
2015-05-29 21:26:00 +0200

    Comittane i config datoteke

---
+++ Kino/Kino/BazaPodataka.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kino
+{
+    public class Baza : DbContext
+    {
+        public Baza()
+            : base("KinoBaza")
+        {
+
+        }
+
+        public DbSet<Artikal> Artiki { get; set; }
+        public DbSet<Cjenovnik> Cjenovnici { get; set; }
+        public DbSet<Film> Filmovi { get; set; }
+        public DbSet<Klijent> Klijenti { get; set; }
+        public DbSet<Korisnik> Korisnici { get; set; }
+        public DbSet<Racun> Racuni { get; set; }
+        public DbSet<Rezervacija> Rezervacije { get; set; }
+        public DbSet<Sala> Sale { get; set; }
+        public DbSet<SedmicniRaspored> SedmicniRasporedi { get; set; }
+        public DbSet<Projekcija> Projekcije { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
