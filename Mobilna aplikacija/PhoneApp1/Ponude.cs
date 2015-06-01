557b52f5b11f3928ab58e53a7f7771736560b381
adnatahic <atahic1@etf.unsa.ba>
2015-06-02 01:01:13 +0200

    mobilna aplikacija

---
+++ Mobilna aplikacija/PhoneApp1/Ponude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneApp1
+{
+    public class Ponude
+    {
+        public string ime { get; set; }
+        public string put_slike { get; set; }
+
+    }
+}
